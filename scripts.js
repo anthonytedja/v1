1b86697c0505c818a01cef48e452602476ccf084
Anthony Tedja <anthonytedja27@gmail.com>
2021-05-17 19:30:56 -0400

    Remove Cookies

--- scripts.js
+++ scripts.js
@@ -99,22 +99,6 @@ window.onload = function () {
   canvasDots();
 };
 
-var checkboxValues = JSON.parse(localStorage.getItem('checkboxValues')) || {},
-  $checkboxes = $(":checkbox");
-
-$checkboxes.on("change", function () {
-  $checkboxes.each(function () {
-    checkboxValues[this.id] = this.checked;
-  });
-
-  localStorage.setItem("checkboxValues", JSON.stringify(checkboxValues));
-});
-
-// On page load
-$.each(checkboxValues, function (key, value) {
-  $("#" + key).prop('checked', value);
-});
-
 function hide() {
   document.getElementById("main").style.display = "none";
   document.getElementById("about").style.display = "none";
