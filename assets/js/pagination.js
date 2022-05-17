9739e8c46d36306ed2b6b6d787d3efeb7da68bf9
Anthony Tedja <anthonytedja27@gmail.com>
2022-05-16 22:55:48 -0400

    Bug Fixes

--- assets/js/pagination.js
+++ assets/js/pagination.js
@@ -1,11 +1,11 @@
 $('#more-projects').click(function () {
-    $(this).hide();
+    $(this).parent().hide();
     $('#projects-row .col-lg-6:hidden').removeClass('aos-animate');
-    $(this).removeClass('aos-animate');
+    $(this).parent().removeClass('aos-animate');
     
     setTimeout(function() {
         $('#projects-row .col-lg-6:hidden').addClass('aos-animate');
-        $(this).addClass('aos-animate');
+        $(this).parent().addClass('aos-animate');
     }, 100);
 
     window.scrollBy(0, 1);
@@ -13,6 +13,6 @@ $('#more-projects').click(function () {
 
     $('#projects-row .col-lg-6:hidden').slice(0, 4).show();
     if ($('#projects-row .col-lg-6').length != $('#projects-row .col-lg-6:visible').length) {
-        $(this).show();
+        $(this).parent().show();
     }
 });
