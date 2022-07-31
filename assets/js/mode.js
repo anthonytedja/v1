6552020ad8a29f5c07f7d9911f065576049a948a
Anthony Tedja <anthonytedja27@gmail.com>
2022-07-31 00:12:46 -0400

    Navbar Bug Fixes

--- assets/js/mode.js
+++ assets/js/mode.js
@@ -18,7 +18,7 @@ function light() {
 	document.documentElement.setAttribute('anthonytedja-data-theme', 'light');
 	changeFavicon('dark');
     $('#code-website-theme').text('"light"');
-	document.getElementById('logo').style.filter = 'invert(10%)';
+	document.getElementById('logo').style.filter = 'invert(12%)';
     document.getElementById('logo').style.transform = 'rotate(0)';
 	document.getElementById('click').style.filter = 'none';
     document.getElementById('hello').style.filter = 'invert(15%)';
@@ -28,7 +28,7 @@ function dark() {
 	document.documentElement.setAttribute('anthonytedja-data-theme', 'dark');
 	changeFavicon('light');
     $('#code-website-theme').text('"dark"');
-	document.getElementById('logo').style.filter = 'invert(85%)';
+	document.getElementById('logo').style.filter = 'invert(80%)';
     document.getElementById('logo').style.transform = 'rotate(180deg)';
 	document.getElementById('click').style.filter = 'invert(90%)';
     document.getElementById('hello').style.filter = 'invert(80%)';
