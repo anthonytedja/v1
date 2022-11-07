6e92f26737357adb30dd8b4b672ad80ba49a4d87
Anthony Tedja <anthonytedja27@gmail.com>
2022-11-07 00:01:39 -0500

    Animate Background & Small Fixes

--- assets/js/clicks.js
+++ assets/js/clicks.js
@@ -70,3 +70,6 @@ $('img').on('dragstart', function(event) { event.preventDefault(); });
 $('img').bind('contextmenu', function(e) {
     return false;
 }); 
+
+// Bypass SVG background animation caching
+$('#frontpage').css('background-image', 'url(/assets/img/background.svg'+ '?' + Math.random() + ')')
