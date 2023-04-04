5f0850872a5eb3fcea79f3664decc367915cf576
Anthony Tedja <anthony.tedja@alida.com>
2023-04-03 21:03:18 -0400

    Update Content

--- assets/js/bs-init.js
+++ assets/js/bs-init.js
@@ -14,6 +14,12 @@ document.addEventListener('DOMContentLoaded', function() {
 		AOS.init();
 	}
 
+	var hoverAnimationTriggerList = [].slice.call(document.querySelectorAll('[data-bss-hover-animate]'));
+	var hoverAnimationList = hoverAnimationTriggerList.forEach(function (hoverAnimationEl) {
+		hoverAnimationEl.addEventListener('mouseenter', function(e){ e.target.classList.add('animated', e.target.dataset.bssHoverAnimate) });
+		hoverAnimationEl.addEventListener('mouseleave', function(e){ e.target.classList.remove('animated', e.target.dataset.bssHoverAnimate) });
+	});
+
 	var toastTriggers = document.querySelectorAll('[data-bs-toggle="toast"]');
 
 	for (var toastTrigger of toastTriggers) {
