37f5ba07f4cd920560300b2aadecedb1f38c061e
Anthony Tedja <anthony.tedja@alida.com>
2023-06-09 00:12:36 -0400

    Optimize Images

--- assets/js/toast.js
+++ assets/js/toast.js
@@ -1,36 +1,43 @@
-let element = document.getElementById('toast-welcome');
+let element = document.getElementById("toast-welcome");
 
 if (element) {
-    const toast = new bootstrap.Toast(element);
+  const toast = new bootstrap.Toast(element);
 
-    let visitCount = parseInt(localStorage.getItem('anthonytedja-visit-count'));
+  let visitCount = parseInt(localStorage.getItem("anthonytedja-visit-count"));
 
-    if (Number.isInteger(visitCount)) {
-        visitCount += 1;
-        $('#toast-welcome-body').text(`Hey! Welcome back for the ${visitCount}${visitCount === 2 ? 'nd' : visitCount === 3 ? 'rd' : 'th'} time :)`);
-    } else {
-        visitCount = 1;
-        toast._config.autohide = false;
-        $('#toast-welcome-body').text("Hey, looks like it's your first time here! Click on the logo to switch themes, or the photo in my bio for a cool animation :)");
-    }
+  if (Number.isInteger(visitCount)) {
+    visitCount += 1;
+    let end = visitCount % 10;
+    $("#toast-welcome-body").text(
+      `Hey! Welcome back for the ${visitCount}${
+        end === 1 ? "st" : end === 2 ? "nd" : end === 3 ? "rd" : "th"
+      } time :)`
+    );
+  } else {
+    visitCount = 1;
+    toast._config.autohide = false;
+    $("#toast-welcome-body").text(
+      "Hey, looks like it's your first time here! Click on the logo to switch themes, or the photo in my bio for a cool animation :)"
+    );
+  }
 
-    localStorage.setItem('anthonytedja-visit-count', visitCount);
+  localStorage.setItem("anthonytedja-visit-count", visitCount);
 
-    setTimeout(() => toast.show(), 3000);
+  setTimeout(() => toast.show(), 3000);
 }
 
-let element1 = document.getElementById('toast-message');
-let element2 = document.getElementById('codeblock-icon-js');
+let element1 = document.getElementById("toast-message");
+let element2 = document.getElementById("codeblock-icon-js");
 let counter = 0;
 
 if (element1 && element2) {
-    const toast1 = new bootstrap.Toast(element1);
-    
-    element2.addEventListener('click', function (event) {
-        counter++;
-        if (counter > 2) {
-            $('#toast-message-body').text("That doesn't do anything dummy");
-            toast1.show();
-        }
-    });
+  const toast1 = new bootstrap.Toast(element1);
+
+  element2.addEventListener("click", function (event) {
+    counter++;
+    if (counter > 2) {
+      $("#toast-message-body").text("That doesn't do anything dummy");
+      toast1.show();
+    }
+  });
 }
