17f60786cf01eb2d515ca37e033b22658181e41b
Anthony Tedja <anthony.tedja@alida.com>
2023-07-07 11:19:43 -0400

    Collapse Navbar on Scroll

--- assets/js/navbar.js
+++ assets/js/navbar.js
@@ -1,80 +1,81 @@
 (function () {
-	"use strict"; // Start of use strict
+  "use strict"; // Start of use strict
 
-	var mainNav = document.querySelector('.navbar');
+  var mainNav = document.querySelector(".navbar");
 
-	if (mainNav) {
+  if (mainNav) {
+    var navbarCollapse = mainNav.querySelector(".navbar-collapse");
 
-		var navbarCollapse = mainNav.querySelector('.navbar-collapse');
+    if (navbarCollapse) {
+      var collapse = new bootstrap.Collapse(navbarCollapse, {
+        toggle: false,
+      });
 
-		if (navbarCollapse) {
+      var navbarItems = navbarCollapse.querySelectorAll("a");
 
-			var collapse = new bootstrap.Collapse(navbarCollapse, {
-				toggle: false
-			});
+      // Closes responsive menu when a scroll trigger link is clicked
 
-			var navbarItems = navbarCollapse.querySelectorAll('a');
+      for (var item of navbarItems) {
+        item.addEventListener("click", function (event) {
+          collapse.hide();
+        });
+      }
 
-			// Closes responsive menu when a scroll trigger link is clicked
+      document
+        .querySelector(".navbar-brand")
+        .addEventListener("click", function (event) {
+          collapse.hide();
+        });
+      document
+        .querySelector(".scroll-to-top")
+        .addEventListener("click", function (event) {
+          collapse.hide();
+        });
+      // Collapse the navbar when page is scrolled
+      window.onscroll = () => collapse.hide();
+    }
 
-			for (var item of navbarItems) {
-				item.addEventListener('click', function (event) {
-					collapse.hide();
-				});
-			}
+    // Collapse Navbar
+    var collapse = new bootstrap.Collapse(navbarCollapse, {
+      toggle: false,
+    });
+    // Collapse now if page is not at top
+    collapse.hide();
+  }
 
-			document.querySelector('.navbar-brand').addEventListener('click', function (event) {
-				collapse.hide();
-			});
-			document.querySelector('.scroll-to-top').addEventListener('click', function (event) {
-				collapse.hide();
-			});
-		}
+  // Scroll to top button appear
+  var scrollToTop = document.querySelector(".scroll-to-top");
+  var navbarbrand = document.querySelector(".navbar-brand");
+  var navbartoggle = document.querySelector(".navbar-toggler");
 
-		// Collapse Navbar
-		var collapse = new bootstrap.Collapse(navbarCollapse, {
-			toggle: false
-		});
-		// Collapse now if page is not at top
-		collapse.hide();
-		// Collapse the navbar when page is scrolled
-		document.addEventListener("scroll", collapse.hide());
-	}
+  if (scrollToTop && navbarbrand && navbartoggle) {
+    // Scroll to top button appear
+    window.addEventListener("scroll", function () {
+      var scrollDistance = window.pageYOffset;
 
-	// Scroll to top button appear
-	var scrollToTop = document.querySelector('.scroll-to-top');
-    var navbarbrand = document.querySelector('.navbar-brand');
-    var navbartoggle = document.querySelector('.navbar-toggler');
+      if (scrollDistance > 100) {
+        //scrollToTop.style.display = 'block';
+        scrollToTop.classList.add("shown");
+        navbarbrand.classList.add("shown");
+        navbartoggle.classList.add("shown");
 
-	if (scrollToTop && navbarbrand && navbartoggle) {
+        scrollToTop.classList.remove("hidden");
+        navbarbrand.classList.remove("hidden");
+        navbartoggle.classList.remove("hidden");
 
-		// Scroll to top button appear
-		window.addEventListener('scroll', function () {
-			var scrollDistance = window.pageYOffset;
+        mainNav.classList.add("navbarblur");
+      } else {
+        //scrollToTop.style.display = 'none';
+        scrollToTop.classList.add("hidden");
+        navbarbrand.classList.add("hidden");
+        navbartoggle.classList.add("hidden");
 
-			if (scrollDistance > 100) {
-				//scrollToTop.style.display = 'block';
-                scrollToTop.classList.add("shown");
-                navbarbrand.classList.add("shown");
-                navbartoggle.classList.add("shown");
-                
-                scrollToTop.classList.remove("hidden");
-                navbarbrand.classList.remove("hidden");
-                navbartoggle.classList.remove("hidden");
-                
-                mainNav.classList.add("navbarblur");
-			} else {
-				//scrollToTop.style.display = 'none';
-                scrollToTop.classList.add("hidden");
-                navbarbrand.classList.add("hidden");
-                navbartoggle.classList.add("hidden");
-                
-                scrollToTop.classList.remove("shown");
-                navbarbrand.classList.remove("shown");
-                navbartoggle.classList.remove("shown");
-                
-                mainNav.classList.remove("navbarblur");
-			}
-		});
-	}
+        scrollToTop.classList.remove("shown");
+        navbarbrand.classList.remove("shown");
+        navbartoggle.classList.remove("shown");
+
+        mainNav.classList.remove("navbarblur");
+      }
+    });
+  }
 })(); // End of use strict
