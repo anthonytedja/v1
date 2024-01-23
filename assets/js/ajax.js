d53dcf1fc29409eca71ca93b7915b6bd52a0dbb3
Anthony Tedja <anthonytedja27@gmail.com>
2024-01-23 18:15:12 -0500

    Update URLs

--- assets/js/ajax.js
+++ assets/js/ajax.js
@@ -1,6 +1,6 @@
 // Ajax request to fetch and render github stats
 $.ajax({
-	url: 'https://api.github.com/repos/anthonytedja/anthonytedja.github.io',
+	url: 'https://api.github.com/repos/anthonytedja/me',
 	method: 'GET',
 	dataType: 'json',
 	success: function (data) {
